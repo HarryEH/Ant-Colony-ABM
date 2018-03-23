9c5bc8d5ae77671ece9926046ca160fbcfbbe7c8
Harry Howarth <hhowarth1@sheffield.ac.uk>
2018-03-23 13:42:33 +0000

    added starter material

---
+++ startup.m
@@ -0,0 +1,8 @@
+addpath(genpath(pwd));
+
+disp("******************************************");
+disp("Welcome to your M&S Assignment Directory");
+disp("Days till assignment due: ");
+disp(daysact(datetime('today'),'14/05/2018'));
+disp("Goodluck");
+disp("******************************************");
