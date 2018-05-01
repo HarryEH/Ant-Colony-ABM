72dd53a5f228d96b02ffd29456713217f36d42d3
Harry Howarth <hhowarth1@sheffield.ac.uk>
2018-05-01 14:28:14 +0100

    changes

--- assignment/Colony.m
+++ assignment/Colony.m
@@ -18,7 +18,7 @@ classdef Colony < handle
                     c.id     = [];
                 case 4 % create a new colony
                     c.pos    = varargin{1};% position of the colony
-                    c.ants   = varargin{2};% number of colony
+                    c.ants   = varargin{2};% anty anty ants
                     c.energy = varargin{3};% energy level of the colony
                     c.id     = varargin{4};% id of the colony
                 otherwise
@@ -26,20 +26,19 @@ classdef Colony < handle
             end
         end
         
-        function ants=generateAnts(self, ratio, size)
-            scouts = size * (1-ratio);
-            ants = Ant.empty(0,0);
+        function generateAnts(self, ratio, size)
+            scouts = double(int16(size * (1-ratio)));
+            self.ants = Ant.empty(0,0);
             for i = 1:1:scouts
-                ants(i) = ScoutAnt(self.pos, self.id);
+                self.ants(i) = ScoutAnt(self.pos, self.id);
             end
             
-            workers = size * ratio;
-            
-            for i = 1:1:workers
-                ants(i) = WorkerAnt(self.pos, self.id);
+            workers = double(int16(size * ratio));
+                        
+            for i = scouts:1:workers
+                self.ants(i) = WorkerAnt(self.pos, self.id);
             end
             
-            ants = ratio;
         end
             
        
