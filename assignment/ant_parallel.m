de006fb8c2a19c3deb12d46bb6ec138fcfe7136b
Harry Howarth <hhowarth1@sheffield.ac.uk>
2018-05-09 15:41:49 +0100

    changes

--- assignment/ant_parallel.m
+++ assignment/ant_parallel.m
@@ -2,21 +2,22 @@
 %Ensure that this is the same number as what you requested from the scheduler
 addpath(genpath(pwd));
 
+ex = 11;
 cpu = 8;
 
-pool = parpool('local',8);
+pool = parpool('local', cpu);
 
 environment_size   = 50;
 colony_count       = 1;
-worker_percentage  = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8];
+worker_percentage  = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0];
 colony_size        = 20;
 simulation_length  = 1000;
 number_experiments = 100;
 
-results = zeros(1,cpu);
-results_two = zeros(1,simulation_length, cpu);
+results = zeros(1,ex);
+results_two = zeros(1,simulation_length, ex);
 
-parfor simu = 1:1:cpu
+parfor simu = 1:1:ex
     
     for i = 1:1:number_experiments
         
@@ -50,13 +51,13 @@ clf;
 
 set(fig, 'Visible', 'off');
 hold on;
-cols = distinguishable_colors(cpu);
-for i = 1:1:cpu
+cols = distinguishable_colors(ex);
+for i = 1:1:ex
     plot(results_two(:,:,i)/number_experiments,'Color',cols(i,:));
 end
 
-legend({'10% Workers','20% Workers','30% Workers','40% Workers', '50% Workers',...
-    '60% Workers','70% Workers','80% Workers'}, 'Location','northwest');
+legend({'0% Workers','10% Workers','20% Workers','30% Workers','40% Workers', '50% Workers',...
+    '60% Workers','70% Workers','80% Workers','90% Workers','100% Workers'}, 'Location','northwest');
 title('Colony Energy vs Iteration for various percentages of Worker Ant');
 xlabel('Iteration');
 ylabel('Colony Energy');
