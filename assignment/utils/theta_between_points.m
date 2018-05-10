75928255bffdafd6985eb340a3b407099249aee2
Harry Howarth <hhowarth1@sheffield.ac.uk>
2018-05-10 11:39:01 +0100

    changes

--- assignment/utils/theta_between_points.m
+++ assignment/utils/theta_between_points.m
@@ -1,6 +1,6 @@
 function [ theta ] = theta_between_points( x, y, a ,b )
 %THETA_BETWEEN_POINTS Summary of this function goes here
 %   Detailed explanation goes here
-    theta = atan2((a - x), (b - y));
+    theta = atan2(double(a - x), double(b - y));
 end
 
